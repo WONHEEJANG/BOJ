bb9790e9eb448b33a0bf07164ec87273f856c608
WONHEEJANG <jangwh93@gmail.com>
2018-10-09 10:23:03 +0900

    [Update] 11729 solved

---
+++ Algorithm/BOJ/DivideConquer/11729.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void solve(int n, int x, int y)
+{
+	if (n == 0)
+		return;
+	solve(n - 1, x, 6 - x - y);
+	cout << x << " " << y << "\n";
+	solve(n - 1, 6 - x - y, y);
+}
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	cout << (1 << N) - 1 << "\n";
+	solve(N, 1, 3);
+
+	return 0;
+}
