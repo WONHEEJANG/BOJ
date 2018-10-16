5da4bc2f68b99c73ab0f5321fee5ab18bcd5ba55
WONHEEJANG <jangwh93@gmail.com>
2018-10-16 22:08:28 +0900

    [Update] 2529 solved using next_permutation

--- Algorithm/BOJ/bruteforce/2529.cpp
+++ Algorithm/BOJ/bruteforce/2529.cpp
@@ -5,8 +5,54 @@
 using namespace std;
 
 vector<char> unequal;
-
+vector<int> m1;
+vector<int> m2;
 int N;
+
+bool check1()
+{
+	for (int i = 0; i < N; i++)
+	{
+		if (unequal[i] == '<')
+		{
+			if (m1[i] > m1[i + 1])
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (m1[i] < m1[i + 1])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool check2()
+{
+	for (int i = 0; i < N; i++)
+	{
+		if (unequal[i] == '<')
+		{
+			if (m2[i] > m2[i + 1])
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (m2[i] < m2[i + 1])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 
 int main()
 {
@@ -18,50 +64,58 @@ int main()
 		cin >> t;
 		unequal.push_back(t);
 	}
-	int cnt1 = 0, cnt2 = 0; // cnt1 < cnt2 >
-	string res = "";
-	string temp = "";
-	string ttemp = "";
-	int m = 9;
-
-	for (int i = 0; i < N; i++)
+	for (int i = 9; i >= 9 - N; i--)
 	{
-		if ((i > 0) && (unequal[i - 1] != unequal[i]))
-		{
-			if (cnt1 > 0)
-			{
-				for (int j = m - cnt1; j <= m; j++)
-				{
-					res += to_string(j);
-				}
-				m = m - cnt1 - 1;
-			}
-			if (cnt2 > 0)
-			{
-				for (int j = m; j > m - cnt2 + 1; j--)
-				{
-					res += to_string(j);
-				}
-				m = m - cnt2 + 1;
-			}
-
-			cnt1 = 0;
-			cnt2 = 0;
-		}
-
-		if (unequal[i] == '<')
-		{
-			cnt1++;
-		}
-		else
-		{
-			cnt2++;
-		}
-
+		m1.push_back(i);
 	}
-	cout << res << endl;
+	for (int i = 0; i <= N; i++)
+	{
+		m2.push_back(i);
+	}
 
+	if (check1())
+	{
+		for (int i = 0; i < m1.size(); i++)
+		{
+			cout << m1[i];
+		}
+		cout << endl;
+	}
 
+	while (prev_permutation(m1.begin(), m1.end()))
+	{
+		if (check1())
+		{
+			for (int i = 0; i < m1.size(); i++)
+			{
+				cout << m1[i];
+			}
+			cout << endl;
+			break;
+		}
+	}
+
+	if (check2())
+	{
+		for (int i = 0; i < m2.size(); i++)
+		{
+			cout << m2[i];
+		}
+		cout << endl;
+	}
+
+	while (next_permutation(m2.begin(), m2.end()))
+	{
+		if (check2())
+		{
+			for (int i = 0; i < m2.size(); i++)
+			{
+				cout << m2[i];
+			}
+			cout << endl;
+			break;
+		}
+	}
 
 	return 0;
 }
